ba5f22a1501f43f4dd91ebd7ceb88def24de8429
Cillian Reidy <X00214986@mytudublin.com>
2025-03-06 19:00:06 +0000

    started login page design

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,21 @@
 from django.views.generic import TemplateView, CreateView
 from .models import CustomUser
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomePageView(CreateView):
 	model = CustomUser
 	template_name = 'home.html'
+	fields = [
+		'username',
+		'email',
+		'password',
+		'premium',
+		'profile_pic'
+		]
+	
+class LoginPageView(CreateView):
+	template_name = 'login.html'
+	model = CustomUser
 	fields = [
 		'username',
 		'email',
