0045e46a77c5020011e7f48b83b460ba27eb0bcc
Vincenzo <vincenzo.joyce2002@gmail.com>
2025-03-07 21:07:44 +0000

    slight adjustment

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import CustomUser
 
-# Create your tests here.
+ACCOUNTS_CREATION = 16000
+
+class ProductModelTest(TestCase):
+    def setUp(self):
+        self.user = []
+        for x in range(ACCOUNTS_CREATION):
+            self.user.append(
+                CustomUser.objects.create(
+                    username='user' + str(x),
+                    email='user'+ str(x) +'@domain.com',
+                    is_superuser=False,
+                    is_premium=True,
+                    phone=str(x)
+                )
+            )
+
+    def test_user_creation(self):
+        for x in range(ACCOUNTS_CREATION):
+            self.assertEqual(self.user[x].username, 'user' + str(x))
+            self.assertEqual(self.user[x].is_superuser, False)
+            self.assertEqual(self.user[x].is_premium, True)
+            self.assertEqual(self.user[x].email, 'user' + str(x) + '@domain.com')
