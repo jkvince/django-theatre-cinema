e356bbde10d9db1ebe70a0a18e58e1ca4fdc4fd6
Vincenzo <vincenzo.joyce2002@gmail.com>
2025-03-12 21:27:07 +0000

    better html and permission block for admin panel

--- customadmin/urls.py
+++ customadmin/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import AdminMainPage
-app_name = 'admin'
+app_name = 'customadmin'
 
 urlpatterns = [
     path('', AdminMainPage.as_view(), name='admin_main'),
--- customadmin/views.py
+++ customadmin/views.py
@@ -1,8 +1,12 @@
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
 
 from accounts.models import CustomUser
 
 
-class AdminMainPage(TemplateView, LoginRequiredMixin):
-	template_name = 'main.html'
+class AdminMainPage(PermissionRequiredMixin, TemplateView):
+	template_name = 'main_admin.html'
+	permission_required = 'accounts.CustomUser'
+	raise_exception = True
+	login_url = 'accounts/login'
