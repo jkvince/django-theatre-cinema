3b851804e5aadd29d569bdf309b6570d0c245e2e
Vincenzo <vincenzo.joyce2002@gmail.com>
2025-03-15 22:23:15 +0000

    added individual pages of users in admin

--- customadmin/urls.py
+++ customadmin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AdminMainView, AdminUserView
+from .views import AdminMainView, AdminUserListView, AdminUserView
 app_name = 'customadmin'
 
 urlpatterns = [
     path('', AdminMainView.as_view(), name='admin_main'),
-    path('users/', AdminUserView.as_view(), name='admin_user'),
+    path('users/', AdminUserListView.as_view(), name='admin_user_list'),
+    path('users/<int:pk>/', AdminUserView.as_view(), name='admin_user'),
 ]
--- customadmin/views.py
+++ customadmin/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.shortcuts import render
@@ -6,20 +6,21 @@ from django.shortcuts import render
 from accounts.models import CustomUser
 
 
-class AdminBaseView(PermissionRequiredMixin):
+class AdminBaseView(PermissionRequiredMixin, TemplateView):
 	# Abstract class for admin pages
 	permission_required = 'accounts.CustomUser'
 	login_url = 'accounts:login' # should redirect to profile first
 
 
-class AdminMainView(AdminBaseView, TemplateView):
+class AdminMainView(AdminBaseView):
 	template_name = 'main.html'
 
-class AdminUserView(AdminBaseView, ListView):
-	template_name = 'user.html'
-	model = CustomUser
-	context_object_name = 'user_list'
-
+class AdminUserListView(AdminBaseView):
 	def get(self, request):
 		context = {'users': CustomUser.objects.all()}
+		return render(request, 'user_list.html', context)
+
+class AdminUserView(AdminBaseView):
+	def get(self, request, pk):
+		context = {'current_user': CustomUser.objects.get(pk=pk)}
 		return render(request, 'user.html', context)
