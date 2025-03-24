e3e33b7388086a902b0ec736b7fcedccc8deeaf0
Vincenzo <vincenzokjoyce@gmail.com>
2025-03-24 16:40:09 +0000

    added models to database

--- theatrecinema/settings.py
+++ theatrecinema/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'customadmin',
     'shopapp',
     'shows',
+    'venues',
 ]
 
 MIDDLEWARE = [
--- venues/models.py
+++ venues/models.py
@@ -5,29 +5,44 @@ class Venue(models.Model):
     venue_id = models.SlugField(
         primary_key=True
     )
-    venue_name = models.CharField()
-    venue_address = models.CharField()
+    venue_name = models.CharField(
+        max_length=30
+    )
+    venue_address = models.CharField(
+        max_length=30
+    )
     venue_accessibility = models.BooleanField()
-    working_hours = models.CharField()
+    working_hours = models.CharField(
+        max_length=10
+    )
     venue_latitude = models.FloatField()
     venue_longitude = models.FloatField()
-    venue_contact = models.EmailField()
+    venue_contact = models.EmailField(
+        blank=True
+    )
 
 
 class Room(models.Model):
-    room_number = models.CharField(
+    room_id = models.SlugField(
         primary_key=True
+    )
+    room_number = models.CharField(
+        max_length=15
     )
     venue_id = models.ForeignKey(Venue, on_delete=models.CASCADE)
     seat_layout = models.FileField()
 
     class Meta:
+        # Compound key
         unique_together = ('room_number', 'venue_id')
 
 
 class Seat(models.Model):
-    seat_number = models.CharField(
+    seat_id = models.SlugField(
         primary_key=True
+    )
+    seat_number = models.CharField(
+        max_length=10
     )
     room_number = models.ForeignKey(Room, on_delete=models.CASCADE)
 
@@ -36,14 +51,15 @@ class Seat(models.Model):
     seat_price = models.FloatField()
 
     class Meta:
+        # Compound key
         unique_together = ('seat_number', 'room_number')
     
 
-class BookedSeat(models.Model):
-    booking_id = models.UUIDField(
-        primary_key=True
-    )
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-    seat_number = models.ForeignKey(Seat, on_delete=models.CASCADE)
-    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
-    booked_status = models.BooleanField()
+#class BookedSeat(models.Model):
+#    booking_id = models.UUIDField(
+#        primary_key=True
+#    )
+#    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+#    seat_number = models.ForeignKey(Seat, on_delete=models.CASCADE)
+#    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+#    booked_status = models.BooleanField()
